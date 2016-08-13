9a21c2790c3fdcc62e439d116c4c10a0a4fd1410
JVProgra <info@jvprogramming.com>
2016-08-12 20:49:43 -0400

    Uploading old project, MyPcPassport

---
+++ MyPcPassport/frmMain.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace MyPcPassport
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.refreshToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.expandAllToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.websiteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.pbProgress = new System.Windows.Forms.ToolStripProgressBar();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.expandAllToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(507, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.refreshToolStripMenuItem,
+            this.exportToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // refreshToolStripMenuItem
+            // 
+            this.refreshToolStripMenuItem.Name = "refreshToolStripMenuItem";
+            this.refreshToolStripMenuItem.Size = new System.Drawing.Size(113, 22);
+            this.refreshToolStripMenuItem.Text = "Refresh";
+            this.refreshToolStripMenuItem.Click += new System.EventHandler(this.refreshToolStripMenuItem_Click);
+            // 
+            // exportToolStripMenuItem
+            // 
+            this.exportToolStripMenuItem.Name = "exportToolStripMenuItem";
+            this.exportToolStripMenuItem.Size = new System.Drawing.Size(113, 22);
+            this.exportToolStripMenuItem.Text = "Export";
+            this.exportToolStripMenuItem.Click += new System.EventHandler(this.exportToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(110, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(113, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // expandAllToolStripMenuItem
+            // 
+            this.expandAllToolStripMenuItem.Name = "expandAllToolStripMenuItem";
+            this.expandAllToolStripMenuItem.Size = new System.Drawing.Size(74, 20);
+            this.expandAllToolStripMenuItem.Text = "Expand All";
+            this.expandAllToolStripMenuItem.Click += new System.EventHandler(this.expandAllToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.websiteToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // websiteToolStripMenuItem
+            // 
+            this.websiteToolStripMenuItem.Name = "websiteToolStripMenuItem";
+            this.websiteToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.websiteToolStripMenuItem.Text = "Website";
+            this.websiteToolStripMenuItem.Click += new System.EventHandler(this.websiteToolStripMenuItem_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.pbProgress});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 282);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 10, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(507, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(55, 17);
+            this.toolStripStatusLabel1.Text = "Progress:";
+            // 
+            // pbProgress
+            // 
+            this.pbProgress.Name = "pbProgress";
+            this.pbProgress.Size = new System.Drawing.Size(75, 16);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Location = new System.Drawing.Point(0, 24);
+            this.treeView1.Margin = new System.Windows.Forms.Padding(2);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(507, 258);
+            this.treeView1.TabIndex = 2;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(507, 304);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MyPcPassport :: JordanHook.com";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripProgressBar pbProgress;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem refreshToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exportToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem websiteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem expandAllToolStripMenuItem;
+    }
+}
+
---
+++ MyPcPassport/frmMain.cs
@@ -0,0 +1,344 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Threading;
+using Microsoft.VisualBasic.Devices;
+using Microsoft.Win32;
+using System.Windows;
+using System.Management;
+using System.IO;
+namespace MyPcPassport
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            refreshToolStripMenuItem.Enabled = false;
+            new Thread(() =>
+            {
+                //Create basic variables 
+                Computer cpu = new Computer();
+                TreeNode Sys = new TreeNode(Environment.UserName + "@" + Environment.MachineName);
+
+                //SOFTWARE NODE 
+                TreeNode OS = new TreeNode("OS");
+                OS.Nodes.Add("Operating System");
+                OS.Nodes.Add("Windows: " + cpu.Info.OSFullName);
+                OS.Nodes.Add("Is64Bit: " + Environment.Is64BitOperatingSystem);
+                OS.Nodes.Add("Service Pack: " + Environment.OSVersion.ServicePack);
+                OS.Nodes.Add("Windows Version: " + cpu.Info.OSVersion);
+                OS.Nodes.Add("Windows Platform: " + cpu.Info.OSPlatform);
+                UpdateProgress(10);
+                Thread.Sleep(150);
+
+                //CPU NODE 
+                TreeNode CPU = new TreeNode("CPU");
+                CPU.Nodes.Add("Central Processing Unit");
+                CPU.Nodes.Add("Name: " + Processor.GetProcessorType());
+                CPU.Nodes.Add("Cores: " + Environment.ProcessorCount);
+                CPU.Nodes.Add("Identifier: " + Processor.GetVendor());
+                CPU.Nodes.Add("Vendor: " + Processor.GetIdentifier());
+                UpdateProgress(20);
+                Thread.Sleep(150);
+
+                //GPU NODE
+                TreeNode GPU = new TreeNode("GPU");
+                GPU.Nodes.Add("Graphics Processing Unit");
+                GPU.Nodes.Add("Name: " + GraphicsProcessor.GetGraphicsCard());
+                UpdateProgress(30);
+                Thread.Sleep(150);
+
+                //RAM NODE 
+                TreeNode RAM = new TreeNode("RAM");
+                RAM.Nodes.Add("Random Access Memory");
+                RAM.Nodes.Add(string.Format("Used RAM: {0:0.00}MB", ((cpu.Info.TotalPhysicalMemory - cpu.Info.AvailablePhysicalMemory) / 1024) / 1024));
+                RAM.Nodes.Add(string.Format("Available RAM: {0:0.00}MB", (cpu.Info.AvailablePhysicalMemory / 1024) / 1024));
+                RAM.Nodes.Add(string.Format("Total RAM: {0:0.00}MB", (cpu.Info.TotalPhysicalMemory / 1024) / 1024));
+                UpdateProgress(40);
+                Thread.Sleep(150);
+
+                //HDD NODE 
+                TreeNode HDD = new TreeNode("HDD");
+                HDD.Nodes.Add("Hard Disk Drives & Other Internal Storage");
+                int count = 0;
+                foreach(DriveInfo di in DriveInfo.GetDrives())
+                {
+                    if(di.DriveType == DriveType.Fixed)
+                    {
+                        count++;
+                        TreeNode slot = new TreeNode("SLOT " + count);
+                        slot.Nodes.Add("Label: " + di.VolumeLabel);
+                        slot.Nodes.Add("Name: " + di.Name);
+                        slot.Nodes.Add("Root: " + di.RootDirectory);
+                        slot.Nodes.Add("Format: " + di.DriveFormat);
+                        slot.Nodes.Add("Available Free Space: " + ((di.AvailableFreeSpace / 1024) / 1024) / 1024 + "GB");
+                        slot.Nodes.Add("Total Free Space: " + ((di.TotalFreeSpace / 1024) / 1024) / 1024 + "GB");
+                        slot.Nodes.Add("Total Space: " + ((di.TotalSize / 1024) / 1024) / 1024 + "GB");
+                        HDD.Nodes.Add(slot);
+                    }
+
+                }
+                UpdateProgress(75);
+                Thread.Sleep(500);
+                
+
+                //Add Nodes to root node 
+                Sys.Nodes.Add(OS);
+                Sys.Nodes.Add(CPU);
+                Sys.Nodes.Add(GPU);
+                Sys.Nodes.Add(RAM);
+                Sys.Nodes.Add(HDD);
+
+                //Complete the system info grabing 
+                Invoke((MethodInvoker)delegate { this.treeView1.Nodes.Add(Sys); refreshToolStripMenuItem.Enabled = true; });
+                UpdateProgress(100);
+            }).Start();
+        }
+
+        public void UpdateProgress(int progress)
+        {
+            if(InvokeRequired)
+                Invoke((MethodInvoker)delegate { UpdateProgress(progress); });
+            else 
+                this.pbProgress.Value = progress;
+        }
+
+        private void refreshToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            refreshToolStripMenuItem.Enabled = false;
+            treeView1.Nodes.Clear();
+            new Thread(() =>
+            {
+                //Create basic variables 
+                Computer cpu = new Computer();
+                TreeNode Sys = new TreeNode(Environment.UserName + "@" + Environment.MachineName);
+
+                //SOFTWARE NODE 
+                TreeNode OS = new TreeNode("OS");
+                OS.Nodes.Add("Operating System");
+                OS.Nodes.Add("Windows: " + cpu.Info.OSFullName);
+                OS.Nodes.Add("Is64Bit: " + Environment.Is64BitOperatingSystem);
+                OS.Nodes.Add("Service Pack: " + Environment.OSVersion.ServicePack);
+                OS.Nodes.Add("Windows Version: " + cpu.Info.OSVersion);
+                OS.Nodes.Add("Windows Platform: " + cpu.Info.OSPlatform);
+                UpdateProgress(10);
+                Thread.Sleep(150);
+
+                //CPU NODE 
+                TreeNode CPU = new TreeNode("CPU");
+                CPU.Nodes.Add("Central Processing Unit");
+                CPU.Nodes.Add("Name: " + Processor.GetProcessorType());
+                CPU.Nodes.Add("Cores: " + Environment.ProcessorCount);
+                CPU.Nodes.Add("Identifier: " + Processor.GetVendor());
+                CPU.Nodes.Add("Vendor: " + Processor.GetIdentifier());
+                UpdateProgress(20);
+                Thread.Sleep(150);
+
+                //GPU NODE
+                TreeNode GPU = new TreeNode("GPU");
+                GPU.Nodes.Add("Graphics Processing Unit");
+                GPU.Nodes.Add("Name: " + GraphicsProcessor.GetGraphicsCard());
+                UpdateProgress(30);
+                Thread.Sleep(150);
+
+                //RAM NODE 
+                TreeNode RAM = new TreeNode("RAM");
+                RAM.Nodes.Add("Random Access Memory");
+                RAM.Nodes.Add(string.Format("Used RAM: {0:0.00}MB", ((cpu.Info.TotalPhysicalMemory - cpu.Info.AvailablePhysicalMemory) / 1024) / 1024));
+                RAM.Nodes.Add(string.Format("Available RAM: {0:0.00}MB", (cpu.Info.AvailablePhysicalMemory / 1024) / 1024));
+                RAM.Nodes.Add(string.Format("Total RAM: {0:0.00}MB", (cpu.Info.TotalPhysicalMemory / 1024) / 1024));
+                UpdateProgress(40);
+                Thread.Sleep(150);
+
+                //HDD NODE 
+                TreeNode HDD = new TreeNode("HDD");
+                HDD.Nodes.Add("Hard Disk Drives & Other Internal Storage");
+                int count = 0;
+                foreach (DriveInfo di in DriveInfo.GetDrives())
+                {
+                    if (di.DriveType == DriveType.Fixed)
+                    {
+                        count++;
+                        TreeNode slot = new TreeNode("SLOT " + count);
+                        slot.Nodes.Add("Label: " + di.VolumeLabel);
+                        slot.Nodes.Add("Name: " + di.Name);
+                        slot.Nodes.Add("Root: " + di.RootDirectory);
+                        slot.Nodes.Add("Format: " + di.DriveFormat);
+                        slot.Nodes.Add("Available Free Space: " + ((di.AvailableFreeSpace / 1024) / 1024) / 1024 + "GB");
+                        slot.Nodes.Add("Total Free Space: " + ((di.TotalFreeSpace / 1024) / 1024) / 1024 + "GB");
+                        slot.Nodes.Add("Total Space: " + ((di.TotalSize / 1024) / 1024) / 1024 + "GB");
+                        HDD.Nodes.Add(slot);
+                    }
+
+                }
+                UpdateProgress(75);
+                Thread.Sleep(500);
+
+
+                //Add Nodes to root node 
+                Sys.Nodes.Add(OS);
+                Sys.Nodes.Add(CPU);
+                Sys.Nodes.Add(GPU);
+                Sys.Nodes.Add(RAM);
+                Sys.Nodes.Add(HDD);
+
+                //Complete the system info grabing 
+                Invoke((MethodInvoker)delegate { this.treeView1.Nodes.Add(Sys); refreshToolStripMenuItem.Enabled = true; });
+                UpdateProgress(100);
+            }).Start();
+            
+        }
+
+        private void exportToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = ".txt|*.txt";
+            if (sfd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                using (System.IO.StreamWriter sw = new StreamWriter(sfd.FileName))
+                {
+                    sw.WriteLine("MyPcPassport 1.0");
+                    sw.WriteLine("(C) JordanHook.com 2014");
+                    sw.WriteLine("---");
+                    sw.WriteLine(treeView1.Nodes[0].Text); //Write pc name 
+
+                    //Loop through all hardware components
+                    foreach(TreeNode node in treeView1.Nodes[0].Nodes)
+                    {
+                        //list the component 
+                        sw.WriteLine("" + node.Text);
+
+                        //List information about said component 
+                        foreach(TreeNode info in node.Nodes)
+                        {
+                            sw.WriteLine("\t" + info.Text);
+
+                            //some components are listed as a secondary node, so lets loop once more... 
+                            foreach(TreeNode n in info.Nodes)
+                            {
+                                sw.WriteLine("\t\t" + n.Text);
+                            }
+                        }
+                    }
+                }
+            }
+            sfd.Dispose();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(-2);
+        }
+
+        private void fAQToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void websiteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //launch website 
+            System.Diagnostics.Process.Start("http://jordanhook.com");
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //short message about the software 
+            MessageBox.Show("MyPcPassport was developed by Jordan Hook of JordanHook.com in C#. The purpose of the application is to be a light weight, system information and dianostics tool in order to help people identify their hardware and system specifications. From time to time, the project will be updated so be sure to check out our website for updates and other cool software!");
+        }
+
+
+        bool expand = true;
+        private void expandAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //use the class variable to detect if it's already expanded and do the opposite 
+            if (expand)
+                treeView1.ExpandAll();
+            else
+                treeView1.CollapseAll();
+
+            //Switch the class variable to match what action needs to be done for next time
+            expand = !expand;
+        }
+    }
+    
+    class GraphicsProcessor
+    {
+        public static string GetGraphicsCard()
+        {
+            try
+            {
+                //Query the system for the graphics card information 
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_DisplayConfiguration");
+                foreach (ManagementObject mo in searcher.Get())
+                {
+                    //if a result is found... check the properties 
+                    foreach (PropertyData property in mo.Properties)
+                    {
+                        //if description is found... 
+                        if (property.Name == "Description")
+                        {
+                            //return the description of the GPU 
+                            return property.Value.ToString();
+                        }
+                    }
+                }
+
+                //other wise return error 
+                return "Error!";
+            }
+            catch { return "Error!"; }
+        }
+
+    }
+
+    class Processor
+    {
+        public static string GetVendor()
+        {
+            try
+            {
+                //Access registry to locate information about the proecessor 
+                RegistryKey RegKey = Registry.LocalMachine;
+                RegKey = RegKey.OpenSubKey("HARDWARE\\DESCRIPTION\\System\\CentralProcessor\\0");
+                return (string)RegKey.GetValue("Identifier");
+            }
+            catch { return "Error!"; }
+        }
+        public static string GetIdentifier()
+        {
+            try
+            {
+                //Access registry to locate information about the proecessor 
+                RegistryKey RegKey = Registry.LocalMachine;
+                RegKey = RegKey.OpenSubKey("HARDWARE\\DESCRIPTION\\System\\CentralProcessor\\0");
+                return (string)RegKey.GetValue("VendorIdentifier");
+            }
+            catch { return "Error!"; }
+        }
+
+        public static string GetProcessorType()
+        {
+            try
+            {
+                //Access registry to locate information about the proecessor 
+                RegistryKey RegKey = Registry.LocalMachine;
+                RegKey = RegKey.OpenSubKey("HARDWARE\\DESCRIPTION\\System\\CentralProcessor\\0");
+                return (string)RegKey.GetValue("ProcessorNameString");
+            }
+            catch { return "Error!"; }
+        }
+    }
+}
